a2d8829df0b462a0b67c789d7b6996a338b05f6b
Ibro <ibro.crnalic@gmail.com>
2023-02-11 22:41:17 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homepage";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="homepage-wrapper"></div>
+    </>
+  );
+}
