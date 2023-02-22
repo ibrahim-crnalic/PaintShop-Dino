4b8e0a1a98f777bf8bb0c9d741d145d11170bd85
Ibro <ibro.crnalic@gmail.com>
2023-02-22 20:09:01 +0100

    hero section

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Hero from "../../components/hero";
+
 
 export default function HomePage() {
-  return <div className="homepage-wrapper"></div>;
+  return (
+    <div className="homepage-wrapper">
+      <Hero />
+    </div>
+  );
 }
